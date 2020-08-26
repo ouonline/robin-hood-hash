68d8ab0fa6a100191115361e8d228ac5a9161f68
Guoyu Ou <benogy@gmail.com>
2020-08-26 20:08:17 +0800

    omake.lua changes

--- tests/omake.lua
+++ tests/omake.lua
@@ -1,21 +1,23 @@
-project = CreateProject()
+project = Project()
+
+flags = {"-Wall", "-Werror", "-Wextra"}
 
 project:CreateBinary("test_1"):AddDependencies(
     project:CreateDependency()
         :AddSourceFiles("test_1.cpp")
-        :AddFlags("-Wall", "-Werror", "-Wextra")
+        :AddFlags(flags)
         :AddIncludeDirectories("../.."))
 
 project:CreateBinary("test_2"):AddDependencies(
     project:CreateDependency()
         :AddSourceFiles("test_2.cpp")
-        :AddFlags("-Wall", "-Werror", "-Wextra")
+        :AddFlags(flags)
         :AddIncludeDirectories("../.."))
 
 project:CreateBinary("test_perf"):AddDependencies(
     project:CreateDependency()
         :AddSourceFiles("test_perf.cpp")
-        :AddFlags("-Wall", "-Werror", "-Wextra")
+        :AddFlags(flags)
         :AddIncludeDirectories("../.."))
 
 return project
